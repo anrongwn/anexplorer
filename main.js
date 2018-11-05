ea04a58cd81dc715a9f62daa31e8c65e6dbc8c67
anrong_wn@126.com <anrong_wn@126.com>
2018-11-05 21:43:36 +0800

    add debug mode

--- main.js
+++ main.js
@@ -5,17 +5,38 @@ const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
 let mainWindow = null;
+const debug = (process.argv.indexOf('--debug')>=0);
 
 function createMainWindow(){
     mainWindow = new BrowserWindow({width: 800, height: 600});
     mainWindow.loadFile('index.html');
+
+    //
+    if (debug){
+        mainWindow.webContents.openDevTools();
+    }
+        
 
     mainWindow.on('closed', ()=>{
         mainWindow = null;
     })
 };
 
-app.on('ready', createMainWindow);
+app.on('ready', ()=>{
+    createMainWindow();
+
+    require('electron').powerMonitor.on('on-battery', ()=>{
+        console.log('pc on battery work mode');
+    });
+
+    require('electron').powerMonitor.on('on-ac', ()=>{
+        console.log('pc on ac work mode');
+    });
+
+    
+})
+
+//app.on('ready', createMainWindow);
 
 app.on('window-all-closed', ()=>{
     if (process.platform!=='darwin'){
@@ -23,8 +44,16 @@ app.on('window-all-closed', ()=>{
     };
 });
 
+/*
 app.on('activate', (event, hasVisibleWindows)=>{
+    
     if ((hasVisibleWindows===false) || (mainWindow===null)){
         createMainWindow();
     }
+    
+});
+*/
+
+app.on('before-quit', ()=>{
+    
 });
